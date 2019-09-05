25d978f79840366ad5a44d6c873e7702339bf6bd
Kevin Tou <toukevin@gmail.com>
2019-09-05 11:45:19 -0700

    Hotfix: changed .listen to read from port

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const server = require('./server.js');
 
 const port = process.env.PORT || 8000;
 
-server.listen(8000, () => console.log(`\n Listening on port ${port} \n`));
+server.listen(port, () => console.log(`\n Listening on port ${port} \n`));
